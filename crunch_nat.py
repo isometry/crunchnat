9a270db3b3cd31ef4764281e67f28c03860ad6dd
Robin Breathe <robin@isometry.net>
2013-03-25 18:50:11 +0000

    - Rename variables for clarity. - Fix off-by-one error. - Add simple stripe mode.

--- crunch_nat.py
+++ crunch_nat.py
@@ -10,65 +10,69 @@ class CrunchError(Exception):
     def __str__(self):
         return 'Excessive Crunch Factor: {}'.format(repr(self.value))
 
-
 class CrunchNAT(object):
-    def __init__(self, public_network, private_network):
-        self.public_network = ipaddr.IPv4Network(public_network)
-        self.private_network = ipaddr.IPv4Network(private_network)
-        self.crunch_factor = self.public_network.prefixlen - self.private_network.prefixlen
+    def __init__(self, external_network, internal_network):
+        self.external_network = ipaddr.IPv4Network(external_network)
+        self.internal_network = ipaddr.IPv4Network(internal_network)
+        self.crunch_factor    = self.external_network.prefixlen - self.internal_network.prefixlen
         if self.crunch_factor > _MAX_CRUNCH_FACTOR:
             raise CrunchError(self.crunch_factor)
-    
+
     @property
-    def dumb_ports_per_host(self):
+    def naive_ports_per_host(self):
         return _PORTS_PER_IP >> self.crunch_factor
 
-    def dumb_pvt2pub(self, _private_address):
-        private_address = ipaddr.IPv4Address(_private_address)
-        private_offset  = int(private_address) - int(self.private_network)
-        public_offset   = private_offset >> self.crunch_factor
-        public_address  = ipaddr.IPv4Address(int(self.public_network) + public_offset)
-        lo_port         = self.dumb_ports_per_host * (private_offset % 2**self.crunch_factor)
-        hi_port         = lo_port + self.dumb_ports_per_host - 1
-        return (public_address, (lo_port, hi_port))
+    def naive_forward(self, _internal_address):
+        internal_address = ipaddr.IPv4Address(_internal_address)
+        internal_offset  = int(internal_address) - int(self.internal_network)
+        external_offset  = internal_offset >> self.crunch_factor
+        external_address = ipaddr.IPv4Address(int(self.external_network) + external_offset)
+        lo_port          = self.naive_ports_per_host * (internal_offset % 2**self.crunch_factor)
+        hi_port          = lo_port + self.naive_ports_per_host - 1
+        return (external_address, (lo_port, hi_port))
 
-    def dumb_pub2pvt(self, _public_address, _port):
-        public_address  = ipaddr.IPv4Address(_public_address)
-        public_offset   = int(public_address) - int(self.public_network)
-        private_offset1 = public_offset << self.crunch_factor
-        private_offset2 = int(_port / self.dumb_ports_per_host)
-        private_address = ipaddr.IPv4Address(int(self.private_network) + private_offset1 + private_offset2)
-        return private_address
-    
+    def naive_reverse(self, _external_address, _port):
+        external_address = ipaddr.IPv4Address(_external_address)
+        external_offset  = int(external_address) - int(self.external_network)
+        internal_offset1 = external_offset << self.crunch_factor
+        internal_offset2 = int(_port / self.naive_ports_per_host)
+        internal_address = ipaddr.IPv4Address(int(self.internal_network) + internal_offset1 + internal_offset2)
+        return internal_address
+
     @property
-    def public_numhosts(self):
-        """ Number of usable public IPs """
-        return self.public_network.numhosts - 2
-    
+    def external_numhosts(self):
+        """ Number of usable external IPs """
+        return self.external_network.numhosts - 2
+
     @property
-    def hosts_per_pub(self):
-        """ Number of private hosts per public IP """
-        return int(self.private_network.numhosts / self.public_numhosts)
-    
+    def hosts_per_external(self):
+        """ Number of internal hosts per external IP """
+        return int(self.internal_network.numhosts / self.external_numhosts) + 1
+
     @property
     def ports_per_host(self):
-        """ Number of public ports per private host """
-        total_ports = self.public_numhosts * (_PORTS_PER_IP - _RESERVED_PORTS)
-        return int(total_ports / self.private_network.numhosts)
+        """ Number of external ports per internal host """
+        return int((_PORTS_PER_IP - _RESERVED_PORTS) / self.hosts_per_external)
 
-    def pvt2pub(self, _private_address):
-        private_address = ipaddr.IPv4Address(_private_address)
-        private_offset  = int(private_address) - int(self.private_network)
-        public_offset   = 1 + int(private_offset / self.hosts_per_pub)
-        public_address  = ipaddr.IPv4Address(int(self.public_network) + public_offset)
-        lo_port         = _RESERVED_PORTS + self.ports_per_host * (private_offset % self.hosts_per_pub)
-        hi_port         = lo_port + self.ports_per_host - 1
-        return (public_address, (lo_port, hi_port))
+    def forward(self, _internal_address, stripe=False):
+        internal_address = ipaddr.IPv4Address(_internal_address)
+        internal_offset  = int(internal_address) - int(self.internal_network)
+        external_offset  = 1 + int(internal_offset / self.hosts_per_external)
+        external_address = ipaddr.IPv4Address(int(self.external_network) + external_offset)
+        if stripe:
+            port_range = range(_RESERVED_PORTS + (internal_offset % self.hosts_per_external), _PORTS_PER_IP, self.hosts_per_external)[:self.ports_per_host]
+            return (external_address, port_range)
+        lo_port = _RESERVED_PORTS + self.ports_per_host * (internal_offset % self.hosts_per_external)
+        hi_port = lo_port + self.ports_per_host - 1
+        return (external_address, (lo_port, hi_port))
 
-    def pub2pvt(self, _public_address, _port):
-        public_address  = ipaddr.IPv4Address(_public_address)
-        public_offset   = int(public_address) - int(self.public_network)
-        private_offset1 = int((public_offset - 1) * self.hosts_per_pub)
-        private_offset2 = int((_port - _RESERVED_PORTS) / self.ports_per_host)
-        private_address = ipaddr.IPv4Address(int(self.private_network) + private_offset1 + private_offset2)
-        return private_address
+    def reverse(self, _external_address, _port, stripe=False):
+        external_address = ipaddr.IPv4Address(_external_address)
+        external_offset  = int(external_address) - int(self.external_network)
+        internal_offset1 = (external_offset - 1) * self.hosts_per_external
+        if stripe:
+            internal_offset2 = (_port - _RESERVED_PORTS) % self.hosts_per_external
+        else:
+            internal_offset2 = (_port - _RESERVED_PORTS) / self.ports_per_host
+        internal_address = ipaddr.IPv4Address(int(self.internal_network) + internal_offset1 + internal_offset2)
+        return internal_address

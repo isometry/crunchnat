c4c74991c722e214b65ecb8498dc4809f0aa5dc9
Robin Breathe <robin@isometry.net>
2016-12-11 22:02:38 +0000

    Whitespace.

--- test.py
+++ test.py
@@ -6,20 +6,20 @@ from crunchnat import CrunchNAT
 class TestSimpleCrunchNAT(unittest.TestCase):
     def setUp(self):
         self.crunch = CrunchNAT('192.0.2.0/24', '10.0.0.0/16', algo='simple')
-    
+
     def test_forward_collisions(self):
         self.assertEqual(self.crunch.check_forward_collisions(), [])
-    
+
     def test_bijective(self):
         self.assertTrue(self.crunch.check_bijection(512))
-    
+
     def test_hosts_per_external(self):
         self.assertEqual(self.crunch.hosts_per_external, 2**(24-16))
-    
+
     def test_ports_per_host(self):
         self.assertEqual(self.crunch.ports_per_host,
                          (2**16-2**10)//2**(24-16))
-    
+
     def test_forward(self):
         forward = self.crunch.forward('10.0.0.10')
         self.assertEqual(forward,
@@ -33,20 +33,20 @@ class TestSimpleCrunchNAT(unittest.TestCase):
 class TestStripeCrunchNAT(unittest.TestCase):
     def setUp(self):
         self.crunch = CrunchNAT('192.0.2.0/24', '10.0.0.0/16', algo='stripe')
-    
+
     def test_forward_collisions(self):
         self.assertEqual(self.crunch.check_forward_collisions(), [])
-    
+
     def test_bijective(self):
         self.assertTrue(self.crunch.check_bijection(512))
-    
+
     def test_hosts_per_external(self):
         self.assertEqual(self.crunch.hosts_per_external, 2**(24-16))
-    
+
     def test_ports_per_host(self):
         self.assertEqual(self.crunch.ports_per_host,
                          (2**16-2**10)//2**(24-16))
-    
+
     def test_forward(self):
         forward = self.crunch.forward('10.0.0.10')
         self.assertEqual(forward,
@@ -60,19 +60,19 @@ class TestStripeCrunchNAT(unittest.TestCase):
 class TestSecureCrunchNAT(unittest.TestCase):
     def setUp(self):
         self.crunch = CrunchNAT('192.0.2.0/24', '10.0.0.0/16', algo='secure')
-    
+
     def test_forward_collisions(self):
         self.assertEqual(self.crunch.check_forward_collisions(), [])
-    
+
     def test_bijective(self):
         self.assertTrue(self.crunch.check_bijection(512))
-    
+
     def test_hosts_per_external(self):
         self.assertEqual(self.crunch.hosts_per_external, 2**(24-16))
-    
+
     def test_ports_per_host(self):
         self.assertEqual(self.crunch.ports_per_host, 251*257//2**(24-16))
-    
+
     def test_forward(self):
         forward = self.crunch.forward('10.0.0.10')
         self.assertEqual(len(forward[1]), self.crunch.ports_per_host)
